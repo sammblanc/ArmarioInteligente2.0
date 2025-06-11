59b1310e582f7aed9356847b04555940463131f7
Samuel Silva <vanderlei.silva@souunit.com.br>
2025-06-11 19:56:46 -0300

    feat: Diminuição de código boilerplate utilizando Lombok.

--- src/main/java/br/com/unit/tokseg/armariointeligente/controller/AuthController.java
+++ src/main/java/br/com/unit/tokseg/armariointeligente/controller/AuthController.java
@@ -1,9 +1,7 @@
 package br.com.unit.tokseg.armariointeligente.controller;
 
-import br.com.unit.tokseg.armariointeligente.model.Usuario;
 import br.com.unit.tokseg.armariointeligente.model.auth.AuthRequest;
 import br.com.unit.tokseg.armariointeligente.model.auth.AuthResponse;
-import br.com.unit.tokseg.armariointeligente.repository.UsuarioRepository;
 import br.com.unit.tokseg.armariointeligente.security.JwtUtils;
 import br.com.unit.tokseg.armariointeligente.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,7 +12,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +26,6 @@ import java.util.Map;
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
-
-    @Autowired
-    UsuarioRepository usuarioRepository;
-
-    @Autowired
-    PasswordEncoder encoder;
 
     @Autowired
     JwtUtils jwtUtils;
@@ -65,7 +56,7 @@ public class AuthController {
                     tipoUsuario));
 
         } catch (AuthenticationException e) {
-            // Captura a falha de autenticação e retorna 401 Unauthorized
+
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("error", "Credenciais inválidas", "message", "O email ou a senha fornecidos estão incorretos."));
         }
